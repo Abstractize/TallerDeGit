47a58c7886bc318676b251fb034a52cf9c4c3e90
Gabriel Abarca Aguilar <gababarca@hotmail.com>
2020-02-22 16:10:36 -0600

    [Add] Bulbasaur

---
+++ src/Pokemon/Bulbasaur.java
@@ -0,0 +1,16 @@
+package Pokemon;
+
+public class Bulbasaur extends Pokemon {
+    public Bulbasaur(){
+        this.setName("Bulbasaur");
+        this.setHp(120);
+        this.setType(Type.Grass);
+    }
+    @Override
+    public void Attack(Pokemon pokemon) {
+        if (pokemon.getType() == Type.Water)
+            pokemon.Attacked(10*2);
+        else
+            super.Attack(pokemon);
+    }
+}
